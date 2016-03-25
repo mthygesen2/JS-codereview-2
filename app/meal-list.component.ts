81270d3c578a76ba35fa458de273dd8c9d013eba
Mao Thygesen <maothygesen@gmail.com>
2016-03-25 09:25:06 -0700

    add new meal form at the top

--- app/meal-list.component.ts
+++ app/meal-list.component.ts
@@ -1,16 +1,19 @@
 import { Component, EventEmitter } from 'angular2/core';
 import { Meal } from './meal.model';
 import { MealComponent } from './meal.component';
-
+import { NewMealComponent } from './new-meal.component';
+import { EditMealDetailsComponent } from './edit-meal-details.component';
 
 @Component({
   selector: 'meal-list',
   inputs: ['mealList'],
-  directives: [MealComponent],
+  directives: [MealComponent, NewMealComponent, EditMealDetailsComponent],
   template: `
-<meal-display *ngFor="#currentMeal of mealList"
-[meal]="currentMeal" >
-</meal-display>
+    <new-meal (onSubmitNewMeal)="createMeal($event)">
+    </new-meal>
+    <meal-display *ngFor="#currentMeal of mealList"
+    [meal]="currentMeal" >
+    </meal-display>
   `
 })
 export class MealListComponent {
---
+++ app/new-meal.component.ts
@@ -0,0 +1,29 @@
+import {Component, EventEmitter} from 'angular2/core';
+import {Meal} from './meal.model';
+
+@Component({
+  selector: 'new-meal',
+  outputs: ['onSubmitNewMeal'],
+  template: `
+  <div class="meal-form">
+    <h2>New Meal</h2>
+    <input placeholder="Meal Name" #newName>
+    <input placeholder="Details of the meal" #newDetails>
+    <input type="number" placeholder="Amount of Calories" min="0" #newCalories >
+    <button (click)="addMeal(newName, newDetails, newCalories)">Add Meal</button>
+  </div>
+  `
+})
+export class NewMealComponent {
+  public onSubmitNewMeal: EventEmitter<String[]>;
+  constructor() {
+    this.onSubmitNewMeal = new EventEmitter();
+  }
+  addMeal(userName: HTMLInputElement, userDetails: HTMLInputElement, userCalories: HTMLInputElement) {
+
+    this.onSubmitNewMeal.emit([userName.value, userDetails.value, userCalories.value]);
+    userName.value = "";
+    userDetails.value = "";
+    userCalories.value = "";
+  }
+}
